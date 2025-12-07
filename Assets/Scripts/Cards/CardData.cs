9357b750b0ec518cdb4e4d266dc8f4c39a334bfb
WistBook <179002176+WistBook@users.noreply.github.com>
2025-12-07 11:27:27 +0900

    Add CardManager

--- Assets/Scripts/Cards/CardData.cs
+++ Assets/Scripts/Cards/CardData.cs
@@ -8,5 +8,8 @@ public class CardData : ScriptableObject
     public string cardName;
     public Sprite cardImage;
     public CardType cardType;
+    [Header("見たら死ぬ回数")]
     public int deathCount = 0;
+    [Header("山札に用意する枚数")]
+    public int initialCount = 1;
 }
