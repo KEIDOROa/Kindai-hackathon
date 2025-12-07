e2c877cacf39cdd587614ed6e9ee2558d9f9342a
WistBook <179002176+WistBook@users.noreply.github.com>
2025-12-07 14:50:55 +0900

    Add CardData

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -1,23 +1,9 @@
 public class Card
 {
     public CardData data;
-    public int currentViewCount = 0;
 
     public Card(CardData data)
     {
         this.data = data;
-    }
-
-    public bool IsDanger()
-    {
-        // ジョーカーだけ判定する
-        if (data.cardType != CardType.Joker) return false;
-
-        return currentViewCount >= data.deathCount;
-    }
-
-    public void View()
-    {
-        currentViewCount++;
     }
 }
